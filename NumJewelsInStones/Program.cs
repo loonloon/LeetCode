bca6a09a0cf90904fa8791839405b56da0061337
40204649 <40204649@CPC402-4649>
2018-07-27 14:50:07 +0800

    Jewels and Stones && To Lower Case

---
+++ NumJewelsInStones/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+namespace NumJewelsInStones
+{
+    class Program
+    {
+        /*
+         * J = jewels,
+         * S = stones you have.  Each character in S is a type of stone you have.
+         * You want to know how many of the stones you have are also jewels.
+         *
+         * The letters in J are guaranteed distinct.
+         * All characters in J and S are letters.
+         * Letters are case sensitive, so "a" is considered a different type of stone from "A".
+         * S and J will consist of letters and have length at most 50.
+         */
+        static void Main(string[] args)
+        {
+            var result = NumJewelsInStones("aA", "aAAbbbb");
+            var result2 = NumJewelsInStones("z", "ZZ");
+        }
+
+        public static int NumJewelsInStones(string J, string S)
+        {
+            if (J.Length == 0 || S.Length == 0)
+            {
+                return 0;
+            }
+
+            var jewels = J.ToCharArray();
+            var stones = S.ToCharArray();
+            return stones.Count(stone => jewels.Contains(stone));
+        }
+    }
+}
---
+++ ToLowerCase/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace ToLowerCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://www.asciitable.com/
+            var result = ToLowerCase("Hello");
+            var result2 = ToLowerCase("here");
+            var result3 = ToLowerCase("LOVELY");
+        }
+
+        public static string ToLowerCase(string str)
+        {
+            var stringBuilder = new StringBuilder();
+
+            foreach (var t in str)
+            {
+                var ascii = (int)t;
+
+                if (ascii >= 65 && ascii <= 90)
+                {
+                    ascii += 32;
+                }
+
+                stringBuilder.Append((char)ascii);
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
