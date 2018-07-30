c25f81981bebd20c9db837df9f3f4555b34b0e1b
40204649 <40204649@CPC402-4649>
2018-07-30 14:31:31 +0800

    ShortesToChar & Tree2str

---
+++ ShortestToChar/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * 821. Shortest Distance to a Character
+ * https://leetcode.com/problems/shortest-distance-to-a-character/description/
+ */
+
+using System;
+
+namespace ShortestToChar
+{
+    class Program
+    {
+        /*
+         * Given a string S and a character C, return an array of integers representing the shortest distance from the character C
+         * in the string.
+         *
+         * Example 1:
+         * Input: S = "loveleetcode", C = 'e'
+         * Output: [3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0]
+         *
+         * Note:
+         * 1. S string length is in [1, 10000].
+         * 2. C is a single character, and guaranteed to be in string S.
+         * 3. All letters in S and C are lowercase.
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = ShortestToChar("loveleetcode", 'e');
+        }
+
+        public static int[] ShortestToChar(string S, char C)
+        {
+            var res = new int[S.Length];
+            var pos = -S.Length;
+
+            for (var i = 0; i < S.Length; ++i)
+            {
+                if (S[i] == C)
+                {
+                    pos = i;
+                }
+
+                res[i] = i - pos;
+            }
+
+            for (var i = S.Length - 1; i >= 0; --i)
+            {
+                if (S[i] == C)
+                {
+                    pos = i;
+                }
+
+                res[i] = Math.Min(res[i], Math.Abs(i - pos));
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Tree2str/Program.cs
@@ -0,0 +1,72 @@
+﻿/*
+ * 606. Construct String from Binary Tree
+ * https://leetcode.com/problems/construct-string-from-binary-tree/description/
+ */
+namespace Tree2str
+{
+    class Program
+    {
+        /*
+         * You need to construct a string consists of parenthesis and integers from a binary tree with the preorder traversing way.
+         *
+         * The null node needs to be represented by empty parenthesis pair "()". And you need to omit all the empty parenthesis pairs
+         * that don't affect the one-to-one mapping relationship between the string and the original binary tree.
+         *
+         * Example 1:
+         * Input: Binary tree: [1,2,3,4]
+         *      1
+         *    /   \
+         *   2     3
+         *  /
+         * 4
+         *
+         * Output: "1(2(4))(3)"
+         *
+         * Explanation:
+         * Originallay it needs to be "1(2(4)())(3()())", but you need to omit all the unnecessary empty parenthesis pairs.
+         * And it will be "1(2(4))(3)".
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = Tree2str(new TreeNode(1) { Left = new TreeNode(2) { Left = new TreeNode(4) }, Right = new TreeNode(3) });
+        }
+
+        public static string Tree2str(TreeNode t)
+        {
+            if (t == null)
+            {
+                return "";
+            }
+
+            var result = $"{t.Val}";
+            var left = Tree2str(t.Left);
+            var right = Tree2str(t.Right);
+
+            if (left == "" && right == "")
+            {
+                return result;
+            }
+
+            if (left == "")
+            {
+                return $"{result}()({right})";
+            }
+
+            if (right == "")
+            {
+                return $"{result}({left})";
+            }
+
+            return $"{result}({left})({right})";
+        }
+    }
+
+    public class TreeNode
+    {
+        public int Val;
+        public TreeNode Left;
+        public TreeNode Right;
+        public TreeNode(int x) { Val = x; }
+    }
+}
