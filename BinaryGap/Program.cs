613f4a5b2a5ff66f73efbf80abfba32ceda523e0
40204649 <40204649@CPC402-4649>
2018-07-31 15:05:36 +0800

    BinaryGap & SubdomainVisits

---
+++ BinaryGap/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+/*
+ * 868. Binary Gap
+ * https://leetcode.com/problems/binary-gap/description/
+ */
+
+namespace BinaryGap
+{
+    class Program
+    {
+        /*
+         * Given a positive integer N, find and return the longest distance between two consecutive 1's in the binary representation of N.
+         * If there aren't two consecutive 1's, return 0.
+         *
+         * Example 1:
+         * Input: 22
+         * Output: 2
+         *
+         * Explanation:
+         * 22 in binary is 0b10110.
+         * In the binary representation of 22, there are three ones, and two consecutive pairs of 1's.
+         * The first consecutive pair of 1's have distance 2.
+         * The second consecutive pair of 1's have distance 1.
+         * The answer is the largest of these two distances, which is 2.
+         *
+         * Example 2:
+         * Input: 5
+         * Output: 2
+         *
+         * Explanation:
+         * 5 in binary is 0b101.
+         *
+         * Example 3:
+         * Input: 6
+         * Output: 1
+         *
+         * Explanation:
+         * 6 in binary is 0b110.
+         *
+         * Example 4:
+         * Input: 8
+         * Output: 0
+         *
+         * Explanation:
+         * 8 in binary is 0b1000.
+         * There aren't any consecutive pairs of 1's in the binary representation of 8, so we return 0.
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = BinaryGap(22);
+            var result2 = BinaryGap(5);
+            var result3 = BinaryGap(6);
+            var result4 = BinaryGap(8);
+        }
+
+        public static int BinaryGap(int N)
+        {
+            var res = 0;
+
+            //Dividing by 2 is equivalent (in this case) to shifting right one place.
+            //Dividing by 4 is equivalent to shifting right two places.
+            //d = -32, represent as 32 bit boundary and value will not affect the result after increment.
+            for (var d = -32; N > 0; N /= 2, d++)
+            {
+                if (N % 2 == 1)
+                {
+                    res = Math.Max(res, d);
+                    d = 0;
+                }
+            }
+
+            return res;
+
+            //var binary = Convert.ToString(N, 2);
+            //var gapList = new List<int>();
+            //var gap = 1;
+
+            //foreach (var bit in binary)
+            //{
+            //    if (!gapList.Any() && bit == '1')
+            //    {
+            //        gapList.Add(0);
+            //    }
+            //    else if (bit == '1')
+            //    {
+            //        gapList.Add(gap);
+            //        gap = 1;
+            //    }
+            //    else
+            //    {
+            //        gap++;
+            //    }
+            //}
+
+            //return gapList.Max();
+        }
+    }
+}
---
+++ SubdomainVisits/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+ * 811. Subdomain Visit Count
+ * https://leetcode.com/problems/subdomain-visit-count/description/
+ */
+
+namespace SubdomainVisits
+{
+    class Program
+    {
+        /*
+         * A website domain like "discuss.leetcode.com" consists of various subdomains. At the top level, we have "com", at the next level,
+         * we have "leetcode.com", and at the lowest level, "discuss.leetcode.com". When we visit a domain like "discuss.leetcode.com",
+         * we will also visit the parent domains "leetcode.com" and "com" implicitly.
+         *
+         * Now, call a "count-paired domain" to be a count (representing the number of visits this domain received),
+         * followed by a space, followed by the address. An example of a count-paired domain might be "9001 discuss.leetcode.com".
+         *
+         * We are given a list cpdomains of count-paired domains. We would like a list of count-paired domains,
+         * (in the same format as the input, and in any order), that explicitly counts the number of visits to each subdomain.
+         *
+         * Example 1:
+         * Input:
+         * ["9001 discuss.leetcode.com"]
+         *
+         * Output:
+         * ["9001 discuss.leetcode.com", "9001 leetcode.com", "9001 com"]
+         *
+         * Explanation:
+         * We only have one website domain: "discuss.leetcode.com". As discussed above, the subdomain "leetcode.com" and
+         * "com" will also be visited. So they will all be visited 9001 times.
+         *
+         * Example 2:
+         * Input:
+         * ["900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"]
+         *
+         * Output:
+         * ["901 mail.com","50 yahoo.com","900 google.mail.com","5 wiki.org","5 org","1 intel.mail.com","951 com"]
+         *
+         * Explanation:
+         * We will visit "google.mail.com" 900 times, "yahoo.com" 50 times, "intel.mail.com" once and "wiki.org" 5 times.
+         * For the subdomains, we will visit "mail.com" 900 + 1 = 901 times, "com" 900 + 50 + 1 = 951 times, and "org" 5 times.
+         *
+         * Notes:
+         * The length of cpdomains will not exceed 100.
+         * The length of each domain name will not exceed 100.
+         * Each address will have either 1 or 2 "." characters.
+         * The input count in any count-paired domain will not exceed 10000.
+         * The answer output can be returned in any order.
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = SubdomainVisits(new[] { "9001 discuss.leetcode.com" });
+            var result2 = SubdomainVisits(new[] { "900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org" });
+        }
+
+        public static IList<string> SubdomainVisits(string[] cpdomains)
+        {
+            var dictionary = new Dictionary<string, int>();
+
+            foreach (var cd in cpdomains)
+            {
+                var splitString = cd.Split(' ');
+                var number = Convert.ToInt32(splitString[0]);
+                var domain = splitString[1];
+                int numberFromDictionary;
+
+                for (var i = 0; i < domain.Length; ++i)
+                {
+                    if (domain[i] != '.')
+                    {
+                        continue;
+                    }
+
+                    var parentDomain = domain.Substring(i + 1);
+
+                    //NET's Dictionary will replace the existing value with the new one if you use [] indexing.
+                    //If you use the Add method, it will instead throw an ArgumentException.
+                    dictionary[parentDomain] = (dictionary.TryGetValue(parentDomain, out numberFromDictionary) ? numberFromDictionary : 0) + number;
+                }
+
+                dictionary[domain] = (dictionary.TryGetValue(domain, out numberFromDictionary) ? numberFromDictionary : 0) + number;
+            }
+
+            var res = new List<string>();
+
+            foreach (var d in dictionary)
+            {
+                res.Add($"{dictionary[d.Key]} {d.Key}");
+            }
+
+            return res;
+        }
+    }
+}
