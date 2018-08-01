1747e8e391dcb33dcf49b044ae349c19775d629a
40204649 <40204649@CPC402-4649>
2018-08-01 15:17:15 +0800

    LeftSimilar & Transpose

---
+++ LeafSimilar/Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+
+/*
+ * 872. Leaf-Similar Trees
+ * https://leetcode.com/problems/leaf-similar-trees/description/
+ */
+
+namespace LeafSimilar
+{
+    class Program
+    {
+        /*
+         * Consider all the leaves of a binary tree.  From left to right order, the values of those leaves form a leaf value sequence.
+         *
+         * For example, in the given tree above, the leaf value sequence is (6, 7, 4, 9, 8).
+         * Two binary trees are considered leaf-similar if their leaf value sequence is the same.
+         * Return true if and only if the two given trees with head nodes root1 and root2 are leaf-similar.
+         *
+         * Note:
+         * Both of the given trees will have between 1 and 100 nodes.
+         *
+         */
+        static void Main(string[] args)
+        {
+            var treeNode = new TreeNode(3)
+            {
+                Left = new TreeNode(5)
+                {
+                    Left = new TreeNode(6),
+                    Right = new TreeNode(2)
+                    {
+                        Left = new TreeNode(7),
+                        Right = new TreeNode(4)
+                    }
+                },
+                Right = new TreeNode(1)
+                {
+                    Left = new TreeNode(9),
+                    Right = new TreeNode(8)
+                }
+            };
+
+            var result = LeafSimilar(treeNode, treeNode);
+        }
+
+        public static bool LeafSimilar(TreeNode root1, TreeNode root2)
+        {
+            var stack = new Stack<TreeNode>();
+            stack.Push(root1);
+
+            var stack2 = new Stack<TreeNode>();
+            stack2.Push(root2);
+
+            while (stack.Count != 0 && stack2.Count != 0)
+            {
+                if (DepthFirstSearch(stack) != DepthFirstSearch(stack2))
+                {
+                    return false;
+                }
+            }
+
+            return stack.Count == 0 && stack2.Count == 0;
+        }
+
+        public static int DepthFirstSearch(Stack<TreeNode> stack)
+        {
+            while (true)
+            {
+                var treeNode = stack.Pop();
+
+                //search from right first
+                if (treeNode.Right != null)
+                {
+                    stack.Push(treeNode.Right);
+                }
+
+                if (treeNode.Left != null)
+                {
+                    stack.Push(treeNode.Left);
+                }
+
+                if (treeNode.Left == null && treeNode.Right == null)
+                {
+                    return treeNode.Val;
+                }
+            }
+        }
+    }
+
+    public class TreeNode
+    {
+        public int Val;
+        public TreeNode Left;
+        public TreeNode Right;
+
+        public TreeNode(int x)
+        {
+            Val = x;
+        }
+    }
+}
---
+++ Transpose/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * 867. Transpose Matrix
+ * https://leetcode.com/problems/transpose-matrix/description/
+ */
+
+namespace Transpose
+{
+    class Program
+    {
+        /*
+         * Given a matrix A, return the transpose of A.
+         * The transpose of a matrix is the matrix flipped over it's main diagonal, switching the row and column indices of the matrix.
+         *
+         * Example 1:
+         * Input: [[1,2,3],[4,5,6],[7,8,9]]
+         * Output: [[1,4,7],[2,5,8],[3,6,9]]
+         *
+         * Example 2:
+         * Input: [[1,2,3],[4,5,6]]
+         * Output: [[1,4],[2,5],[3,6]]
+         *
+         * Note:
+         * 1 <= A.length <= 1000
+         * 1 <= A[0].length <= 1000
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = Transpose(new[] { new[] { 1, 2, 3 }, new[] { 4, 5, 6 }, new[] { 7, 8, 9 } });
+            var result2 = Transpose(new[] { new[] { 1, 2, 3 }, new[] { 4, 5, 6 } });
+        }
+
+        public static int[][] Transpose(int[][] A)
+        {
+            var row = A.GetLength(0);
+            var column = A[0].GetLength(0);
+            var transpose = new int[column][];
+
+            for (var i = 0; i < row; i++)
+            {
+                for (var j = 0; j < column; j++)
+                {
+                    if (transpose[j] == null)
+                    {
+                        transpose[j] = new int[row];
+                    }
+
+                    transpose[j][i] = A[i][j];
+                }
+            }
+
+            return transpose;
+        }
+    }
+}
