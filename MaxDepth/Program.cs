5864cd730cf716dde44f82fbba7abf8b237e48d0
40204649 <40204649@CPC402-4649>
2018-08-14 14:35:52 +0800

    IsToeplitzMatrix & MaxDepth & UncommonFromSentences

---
+++ MaxDepth/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+ * 559. Maximum Depth of N-ary Tree
+ * https://leetcode.com/problems/maximum-depth-of-n-ary-tree/description/
+ *
+ */
+namespace MaxDepth
+{
+    class Program
+    {
+        /*
+         * Given a n-ary tree, find its maximum depth.
+         * The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+         * For example, given a 3-ary tree:
+         *
+         *         1
+               /   |   \
+              3    2    4
+             / \
+            5   6
+         *
+         * We should return its max depth, which is 3.
+         *
+         * Note:
+         * The depth of the tree is at most 1000.
+         * The total number of nodes is at most 5000.
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = MaxDepth(new Node(1, new List<Node>
+            {
+                new Node(3, new List<Node>
+                {
+                    new Node(5, null),
+                    new Node(6, null)
+                }),
+                new Node(2, null),
+                new Node(4, null)
+            }));
+        }
+
+        public static int MaxDepth(Node root)
+        {
+            var max = 0;
+
+            if (root == null)
+            {
+                return max;
+            }
+
+            foreach (var childNode in root.Children)
+            {
+                max = Math.Max(max, MaxDepth(childNode));
+            }
+
+            return max + 1;
+        }
+    }
+
+    public class Node
+    {
+        public int Val;
+        public IList<Node> Children;
+
+        public Node(int val, IList<Node> children)
+        {
+            Val = val;
+            Children = children;
+        }
+    }
+}
---
+++ UncommonFromSentences/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 888. Uncommon Words from Two Sentences
+ * https://leetcode.com/problems/uncommon-words-from-two-sentences/description/
+ */
+namespace UncommonFromSentences
+{
+    class Program
+    {
+        /*
+         * We are given two sentences A and B.  (A sentence is a string of space separated words.  Each word consists only of lowercase letters.)
+         * A word is uncommon if it appears exactly once in one of the sentences, and does not appear in the other sentence.
+         *
+         * Return a list of all uncommon words.
+         * You may return the list in any order.
+         *
+         * Example 1:
+         * Input: A = "this apple is sweet", B = "this apple is sour"
+         * Output: ["sweet","sour"]
+         *
+         * Example 2:
+         * Input: A = "apple apple", B = "banana"
+         * Output: ["banana"]
+         *
+         * Note:
+         * 1. 0 <= A.length <= 200
+         * 2. 0 <= B.length <= 200
+         * 3. A and B both contain only spaces and lowercase letters.
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = UncommonFromSentences("this apple is sweet", "this apple is sour");
+            var result2 = UncommonFromSentences("apple apple", "banana");
+        }
+
+        public static string[] UncommonFromSentences(string A, string B)
+        {
+            var dictionary = new Dictionary<string, int>();
+            var words = $"{A} {B}".Split(' ');
+
+            foreach (var word in words)
+            {
+                int count;
+
+                if (dictionary.TryGetValue(word, out count))
+                {
+                    dictionary[word] = count + 1;
+                }
+                else
+                {
+                    dictionary.Add(word, 1);
+                }
+            }
+
+            var uncommonList = new List<string>();
+
+            foreach (var key in dictionary.Keys)
+            {
+                if (dictionary[key] == 1)
+                {
+                    uncommonList.Add(key);
+                }
+            }
+
+            return uncommonList.ToArray();
+        }
+    }
+}
