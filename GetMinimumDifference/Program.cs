5532b0aec21758dd37a22487d88925ff8095b5bf
40204649 <40204649@CPC402-4649>
2018-08-23 14:49:59 +0800

    GetMinimumDifference & MinMoves

---
+++ GetMinimumDifference/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+/*
+ * 530. Minimum Absolute Difference in BST
+ * https://leetcode.com/problems/minimum-absolute-difference-in-bst/description/
+ */
+namespace GetMinimumDifference
+{
+    class Program
+    {
+        /*
+         * Given a binary search tree with non-negative values, find the minimum absolute difference between values of any two nodes.
+         * Example:
+         *
+         * Input:
+         * 1
+         *  \
+         *   3
+         *  /
+         * 2
+         *
+         * Output:
+         * 1
+         *
+         * Explanation:
+         * The minimum absolute difference is 1, which is the difference between 2 and 1 (or between 2 and 3).
+         *
+         * Note: There are at least two nodes in this BST.
+         */
+
+        private static int _min = int.MaxValue;
+        private static int? _prev;
+
+        static void Main(string[] args)
+        {
+            var result = GetMinimumDifference(new TreeNode(1)
+            {
+                Right = new TreeNode(3) { Left = new TreeNode(2) }
+            });
+        }
+
+        public static int GetMinimumDifference(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            GetMinimumDifference(root.Left);
+
+            if (_prev != null)
+            {
+                _min = Math.Min(_min, root.Val - (int)_prev);
+            }
+
+            _prev = root.Val;
+            GetMinimumDifference(root.Right);
+            return _min;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int Val;
+        public TreeNode Left;
+        public TreeNode Right;
+
+        public TreeNode(int x)
+        {
+            Val = x;
+        }
+    }
+}
---
+++ MinMoves/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+
+/*
+ * 453. Minimum Moves to Equal Array Elements
+ * https://leetcode.com/problems/minimum-moves-to-equal-array-elements/description/
+ */
+namespace MinMoves
+{
+    class Program
+    {
+        /*
+         * Given a non-empty integer array of size n, find the minimum number of moves required to make all array elements equal,
+         * where a move is incrementing n - 1 elements by 1.
+         *
+         * Example:
+         * Input:
+         * [1,2,3]
+         *
+         * Output:
+         * 3
+         *
+         * Explanation:
+         * Only three moves are needed (remember each move increments two elements):
+         * [1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = MinMoves(new[] { 1, 2, 3 });
+        }
+
+        public static int MinMoves(int[] nums)
+        {
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+
+            var min = nums.Min();
+            return nums.Sum(num => num - min);
+        }
+    }
+}
