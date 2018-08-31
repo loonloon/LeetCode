49337b5057aca5eb1ecec2b783a7e7ef8fc455f6
40204649 <40204649@CPC402-4649>
2018-08-31 15:26:23 +0800

    FindDisappearedNumbers & MajorityElement

---
+++ FindDisappearedNumbers/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+ * 448. Find All Numbers Disappeared in an Array
+ * https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/description/
+ */
+namespace FindDisappearedNumbers
+{
+    class Program
+    {
+        /*
+         * Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+         * Find all the elements of [1, n] inclusive that do not appear in this array.
+         * Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+         *
+         * Example:
+         * Input:
+         * [4,3,2,7,8,2,3,1]
+         *
+         * Output:
+         * [5,6]
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = FindDisappearedNumbers(new[] { 4, 3, 2, 7, 8, 2, 3, 1 });
+        }
+
+        public static IList<int> FindDisappearedNumbers(int[] nums)
+        {
+            var list = new List<int>();
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var val = Math.Abs(nums[i]) - 1;
+
+                if (nums[val] > 0)
+                {
+                    nums[val] = -nums[val];
+                }
+            }
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > 0)
+                {
+                    list.Add(i + 1);
+                }
+            }
+
+            return list;
+        }
+    }
+}
---
+++ MajorityElement/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+/*
+ * 169. Majority Element
+ * https://leetcode.com/problems/majority-element/description/
+ */
+namespace MajorityElement
+{
+    class Program
+    {
+        /*
+         * Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+         * You may assume that the array is non-empty and the majority element always exist in the array.
+         *
+         * Example 1:
+         * Input: [3,2,3]
+         * Output: 3
+         *
+         * Example 2:
+         * Input: [2,2,1,1,1,2,2]
+         * Output: 2
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = MajorityElement(new[] { 3, 2, 3 });
+            var result2 = MajorityElement(new[] { 2, 2, 1, 1, 1, 2, 2 });
+        }
+
+        public static int MajorityElement(int[] nums)
+        {
+            var dictionary = new Dictionary<int, int>();
+
+            foreach (var num in nums)
+            {
+                if (dictionary.ContainsKey(num))
+                {
+                    dictionary[num] += 1;
+                }
+                else
+                {
+                    dictionary.Add(num, 1);
+                }
+            }
+
+            return dictionary.OrderByDescending(x => x.Value).First().Key;
+        }
+    }
+}
