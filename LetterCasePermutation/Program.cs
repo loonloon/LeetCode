8a19bb1fe00d317f323dd566ff65d8bf2f9539e8
40204649 <40204649@CPC402-4649>
2018-09-03 15:56:11 +0800

    LetterCasePermutation & RomanToInt

---
+++ LetterCasePermutation/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+/*
+ * 784. Letter Case Permutation
+ * https://leetcode.com/problems/letter-case-permutation/
+ */
+namespace LetterCasePermutation
+{
+    class Program
+    {
+        /*
+         * Given a string S, we can transform every letter individually to be lowercase or uppercase to create another string.
+         * Return a list of all possible strings we could create.
+         *
+         * Examples:
+         * Input: S = "a1b2"
+         * Output: ["a1b2", "a1B2", "A1b2", "A1B2"]
+         *
+         * Input: S = "3z4"
+         * Output: ["3z4", "3Z4"]
+         *
+         * Input: S = "12345"
+         * Output: ["12345"]
+         *
+         * Note:
+         * S will be a string with length at most 12.
+         * S will consist only of letters or digits.
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = LetterCasePermutation("a1b2");
+            var result2 = LetterCasePermutation("3z4");
+            var result3 = LetterCasePermutation("12345");
+        }
+
+        public static IList<string> LetterCasePermutation(string S)
+        {
+            if (S == null)
+            {
+                return new List<string>();
+            }
+
+            var queue = new Queue<string>();
+            queue.Enqueue(S);
+
+            for (var i = 0; i < S.Length; i++)
+            {
+                if (char.IsDigit(S[i]))
+                {
+                    continue;
+                }
+
+                var size = queue.Count;
+
+                for (var j = 0; j < size; j++)
+                {
+                    var word = queue.Dequeue();
+                    var chars = word.ToCharArray();
+
+                    chars[i] = char.ToUpper(chars[i]);
+                    queue.Enqueue(new string(chars));
+
+                    chars[i] = char.ToLower(chars[i]);
+                    queue.Enqueue(new string(chars));
+                }
+            }
+
+            return queue.ToList();
+        }
+    }
+}
---
+++ RomanToInt/Program.cs
@@ -0,0 +1,106 @@
+﻿/*
+ * 13. Roman to Integer
+ * https://leetcode.com/problems/roman-to-integer/description/
+ */
+namespace RomanToInt
+{
+    class Program
+    {
+        /*
+         * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+         * Symbol       Value
+         * I             1
+         * V             5
+         * X             10
+         * L             50
+         * C             100
+         * D             500
+         * M             1000
+         *
+         * For example, two is written as II in Roman numeral, just two one's added together.
+         * Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII, which is XX + V + II.
+         *
+         * Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII.
+         * Instead, the number four is written as IV. Because the one is before the five we subtract it making four.
+         * The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+         * I can be placed before V (5) and X (10) to make 4 and 9.
+         * X can be placed before L (50) and C (100) to make 40 and 90.
+         * C can be placed before D (500) and M (1000) to make 400 and 900.
+         *
+         * Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
+         *
+         * Example 1:
+         * Input: "III"
+         * Output: 3
+         *
+         * Example 2:
+         * Input: "IV"
+         * Output: 4
+         *
+         * Example 3:
+         * Input: "IX"
+         * Output: 9
+         *
+         * Example 4:
+         * Input: "LVIII"
+         * Output: 58
+         * Explanation: C = 100, L = 50, XXX = 30 and III = 3.
+         *
+         * Example 5:
+         * Input: "MCMXCIV"
+         * Output: 1994
+         * Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = RomanToInt("III");
+            var result2 = RomanToInt("IV");
+            var result3 = RomanToInt("IX");
+            var result4 = RomanToInt("LVIII");
+            var result5 = RomanToInt("MCMXCIV");
+        }
+
+        public static int RomanToInt(string s)
+        {
+            var total = 0;
+
+            if (s.Contains("IV")) { total -= 2; }
+            if (s.Contains("IX")) { total -= 2; }
+            if (s.Contains("XL")) { total -= 20; }
+            if (s.Contains("XC")) { total -= 20; }
+            if (s.Contains("CD")) { total -= 200; }
+            if (s.Contains("CM")) { total -= 200; }
+
+            foreach (var character in s)
+            {
+                switch (character)
+                {
+                    case 'M':
+                        total += 1000;
+                        break;
+                    case 'D':
+                        total += 500;
+                        break;
+                    case 'C':
+                        total += 100;
+                        break;
+                    case 'L':
+                        total += 50;
+                        break;
+                    case 'X':
+                        total += 10;
+                        break;
+                    case 'V':
+                        total += 5;
+                        break;
+                    case 'I':
+                        total += 1;
+                        break;
+                }
+            }
+
+            return total;
+        }
+    }
+}
