8adcce3cfabc8a4897f609dd491d13b4adc3e815
40204649 <40204649@CPC402-4649>
2018-09-05 14:51:22 +0800

    DeleteDuplicates & DeleteNode & ReverseList

---
+++ DeleteDuplicates/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace DeleteDuplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = DeleteDuplicates(new ListNode(1) { Next = new ListNode(1) { Next = new ListNode(1) } });
+            var result2 = DeleteDuplicates(new ListNode(1) { Next = new ListNode(1) { Next = new ListNode(2) { Next = new ListNode(3) { Next = new ListNode(3) } } } });
+        }
+
+        public static ListNode DeleteDuplicates(ListNode head)
+        {
+            var currentNode = head;
+
+            while (currentNode != null)
+            {
+                if (currentNode.Next != null && currentNode.Val == currentNode.Next.Val)
+                {
+                    currentNode.Next = currentNode.Next.Next;
+                }
+                else
+                {
+                    currentNode = currentNode.Next;
+                }
+            }
+
+            return head;
+        }
+    }
+
+    public class ListNode
+    {
+        public int Val;
+        public ListNode Next;
+
+        public ListNode(int x)
+        {
+            Val = x;
+        }
+    }
+}
---
+++ ReverseList/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * 206. Reverse Linked List
+ * https://leetcode.com/problems/reverse-linked-list/description/
+ */
+namespace ReverseList
+{
+    class Program
+    {
+        /*
+         * Reverse a singly linked list.
+         *
+         * Example:
+         * Input: 1->2->3->4->5->NULL
+         * Output: 5->4->3->2->1->NULL
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = ReverseList(new ListNode(1)
+            {
+                Next = new ListNode(2) { Next = new ListNode(3) { Next = new ListNode(4) { Next = new ListNode(5) } } }
+            });
+        }
+
+        public static ListNode ReverseList(ListNode head)
+        {
+            ListNode newHead = null;
+
+            while (head != null)
+            {
+                var next = head.Next;
+                head.Next = newHead;
+                newHead = head;
+                head = next;
+            }
+
+            return newHead;
+        }
+    }
+
+    public class ListNode
+    {
+        public int Val;
+        public ListNode Next;
+
+        public ListNode(int x)
+        {
+            Val = x;
+        }
+    }
+}
