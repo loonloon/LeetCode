5c1a7364d92c1cb6e9da5acca1f09db8aada04ca
40204649 <40204649@CPC402-4649>
2018-09-12 14:34:49 +0800

    CountSegments & LongestWord & RemoveDuplicates

---
+++ CountSegments/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * 434. Number of Segments in a String
+ * https://leetcode.com/problems/number-of-segments-in-a-string/description/
+ */
+namespace CountSegments
+{
+    class Program
+    {
+        /*
+         * Count the number of segments in a string, where a segment is defined to be a contiguous sequence of non-space characters.
+         * Please note that the string does not contain any non-printable characters.
+         *
+         * Example:
+         * Input: "Hello, my name is John"
+         * Output: 5
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = CountSegments("Hello, my name is John");
+        }
+
+        public static int CountSegments(string s)
+        {
+            var wordCount = 0;
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                //if character is not spacing AND
+                //if index is 0, is words OR the spacing is before the character 
+                if (s[i] != ' ' && (i == 0 || s[i - 1] == ' '))
+                {
+                    wordCount++;
+                }
+            }
+
+            return wordCount;
+        }
+    }
+}
---
+++ LongestWord/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+/*
+ * 720. Longest Word in Dictionary
+ * https://leetcode.com/problems/longest-word-in-dictionary/description/
+ */
+namespace LongestWord
+{
+    class Program
+    {
+        /*
+         * Given a list of strings words representing an English Dictionary,
+         * find the longest word in words that can be built one character at a time by other words in words. If there is more than one possible answer, return the longest word with the smallest lexicographical order.
+         *
+         * If there is no answer, return the empty string.
+         * Example 1:
+         * Input:
+         * words = ["w","wo","wor","worl", "world"]
+         * Output: "world"
+         *
+         * Explanation:
+         * The word "world" can be built one character at a time by "w", "wo", "wor", and "worl".
+         *
+         * Example 2:
+         * Input:
+         * words = ["a", "banana", "app", "appl", "ap", "apply", "apple"]
+         * Output: "apple"
+         *
+         * Explanation:
+         * Both "apply" and "apple" can be built from other words in the dictionary.
+         * However, "apple" is lexicographically smaller than "apply".
+         *
+         * Note:
+         * All the strings in the input will only contain lowercase letters.
+         * The length of words will be in the range [1, 1000].
+         * The length of words[i] will be in the range [1, 30].
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = LongestWord(new[] { "w", "wo", "wor", "worl", "world" });
+            var result2 = LongestWord(new[] { "a", "banana", "app", "appl", "ap", "apply", "apple" });
+        }
+
+        public static string LongestWord(string[] words)
+        {
+            Array.Sort(words);
+            var hashSet = new List<string>();
+            var longestWord = string.Empty;
+
+            foreach (var word in words)
+            {
+                if (word.Length == 1 || hashSet.Contains(word.Substring(0, word.Length - 1)))
+                {
+                    longestWord = word.Length > longestWord.Length ? word : longestWord;
+                    hashSet.Add(word);
+                }
+            }
+
+            return longestWord;
+        }
+    }
+}
