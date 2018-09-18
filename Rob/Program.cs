361018d98bb1e94ef85d328cc1857cb2e1ba40bd
40204649 <40204649@CPC402-4649>
2018-09-18 16:23:31 +0800

    Rob

---
+++ Rob/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+/*
+ * 198. House Robber
+ * https://leetcode.com/problems/house-robber/description/
+ */
+namespace Rob
+{
+    class Program
+    {
+        /*
+         * You are a professional robber planning to rob houses along a street.
+         *
+         * Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses
+         * have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+         *
+         * Given a list of non-negative integers representing the amount of money of each house,
+         * determine the maximum amount of money you can rob tonight without alerting the police.
+         *
+         * Example 1:
+         * Input: [1,2,3,1]
+         * Output: 4
+         * Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3). Total amount you can rob = 1 + 3 = 4.
+         *
+         * Example 2:
+         * Input: [2,7,9,3,1]
+         * Output: 12
+         * Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1). Total amount you can rob = 2 + 9 + 1 = 12.
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = Rob(new[] { 1, 2, 3, 1 });
+            var result2 = Rob(new[] { 2, 7, 9, 3, 1 });
+        }
+
+        public static int Rob(int[] nums)
+        {
+            var a = 0;
+            var b = 0;
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    a = Math.Max(a + nums[i], b);
+                }
+                else
+                {
+                    b = Math.Max(a, b + nums[i]);
+                }
+            }
+
+            return Math.Max(a, b);
+        }
+    }
+}
