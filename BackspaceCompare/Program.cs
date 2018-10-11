254bb8b2a1ce775309e0ff42fcb9e97a5f052db8
Hon Loon <tenghonloon@hotmail.com>
2018-10-12 00:06:31 +0800

    BackspaceCompare & NextGreatestLetter

---
+++ BackspaceCompare/Program.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * 844. Backspace String Compare
+ */
+namespace BackspaceCompare
+{
+    class Program
+    {
+        /*
+         * Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+         *
+         * Example 1:
+         * Input: S = "ab#c", T = "ad#c"
+         * Output: true
+         * Explanation: Both S and T become "ac".
+         *
+         * Example 2:
+         * Input: S = "ab##", T = "c#d#"
+         * Output: true
+         * Explanation: Both S and T become "".
+         *
+         * Example 3:
+         * Input: S = "a##c", T = "#a#c"
+         * Output: true
+         * Explanation: Both S and T become "c".
+         *
+         * Example 4:
+         * Input: S = "a#c", T = "b"
+         * Output: false
+         * Explanation: S becomes "c" while T becomes "b".
+         *
+         * Note:
+         * 1 <= S.length <= 200
+         * 1 <= T.length <= 200
+         * S and T only contain lowercase letters and '#' characters.
+         *
+         * Follow up:
+         * Can you solve it in O(N) time and O(1) space?
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = BackspaceCompare("ab#c", "ad#c");
+            var result2 = BackspaceCompare("ab##", "c#d#");
+            var result3 = BackspaceCompare("a##c", "#a#c");
+            var result4 = BackspaceCompare("a#c", "b");
+        }
+
+
+        public static bool BackspaceCompare(string S, string T)
+        {
+            var sLength = S.Length - 1;
+            var tLength = T.Length - 1;
+
+            while (true)
+            {
+                for (var back = 0; sLength >= 0 && (back > 0 || S[sLength] == '#'); --sLength)
+                {
+                    back += S[sLength] == '#' ? 1 : -1;
+                }
+
+                for (var back = 0; tLength >= 0 && (back > 0 || T[tLength] == '#'); --tLength)
+                {
+                    back += T[tLength] == '#' ? 1 : -1;
+                }
+
+                if (sLength >= 0 && tLength >= 0 && S[sLength] == T[tLength])
+                {
+                    sLength--;
+                    tLength--;
+                }
+                else
+                {
+                    return sLength == -1 && tLength == -1;
+                }
+            }
+        }
+    }
+}
---
+++ NextGreatestLetter/Program.cs
@@ -0,0 +1,76 @@
+﻿/*
+ * 744. Find Smallest Letter Greater Than Target
+ * https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+ */
+namespace NextGreatestLetter
+{
+    class Program
+    {
+        /*
+         * Given a list of sorted characters letters containing only lowercase letters, and given a target letter target,
+         * find the smallest element in the list that is larger than the given target.
+         *
+         * Letters also wrap around. For example, if the target is target = 'z' and letters = ['a', 'b'], the answer is 'a'.
+         *
+         * Examples:
+         * Input:
+         * letters = ["c", "f", "j"]
+         * target = "a"
+         * Output: "c"
+         *
+         * Input:
+         * letters = ["c", "f", "j"]
+         * target = "c"
+         * Output: "f"
+         *
+         * Input:
+         * letters = ["c", "f", "j"]
+         * target = "d"
+         * Output: "f"
+         *
+         * Input:
+         * letters = ["c", "f", "j"]
+         * target = "g"
+         * Output: "j"
+         *
+         * Input:
+         * letters = ["c", "f", "j"]
+         * target = "j"
+         * Output: "c"
+         *
+         * Input:
+         * letters = ["c", "f", "j"]
+         * target = "k"
+         * Output: "c"
+         *
+         * Note:
+         * letters has a length in range [2, 10000].
+         * letters consists of lowercase letters, and contains at least 2 unique letters.
+         * target is a lowercase letter.
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = NextGreatestLetter(new[] { 'c', 'f', 'j' }, 'a');
+            var result2 = NextGreatestLetter(new[] { 'c', 'f', 'j' }, 'c');
+            var result3 = NextGreatestLetter(new[] { 'c', 'f', 'j' }, 'd');
+            var result4 = NextGreatestLetter(new[] { 'c', 'f', 'j' }, 'g');
+            var result5 = NextGreatestLetter(new[] { 'c', 'f', 'j' }, 'j');
+            var result6 = NextGreatestLetter(new[] { 'c', 'f', 'j' }, 'k');
+        }
+
+        public static char NextGreatestLetter(char[] letters, char target)
+        {
+            //can use binary search if the data is in ascending order
+            foreach (var letter in letters)
+            {
+                if (letter > target)
+                {
+                    return letter;
+                }
+            }
+
+            return letters[0];
+        }
+    }
+}
