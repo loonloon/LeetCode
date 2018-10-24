cfd8290f9af15ba4333c811a9ae1f11784badbff
Hon Loon <tenghonloon@hotmail.com>
2018-10-25 00:13:17 +0800

    FindLengthOfLCIS & FindShortestSubArray

---
+++ FindLengthOfLCIS/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * 674. Longest Continuous Increasing Subsequence
+ * https://leetcode.com/problems/longest-continuous-increasing-subsequence/
+ */
+
+using System;
+
+namespace FindLengthOfLCIS
+{
+    class Program
+    {
+        /*
+         * Given an unsorted array of integers, find the length of longest continuous increasing subsequence (subarray).
+         *
+         * Example 1:
+         * Input: [1,3,5,4,7]
+         * Output: 3
+         * Explanation: The longest continuous increasing subsequence is [1,3,5], its length is 3.
+         * Even though [1,3,5,7] is also an increasing subsequence, it's not a continuous one where 5 and 7 are separated by 4.
+         *
+         * Example 2:
+         * Input: [2,2,2,2,2]
+         * Output: 1
+         * Explanation: The longest continuous increasing subsequence is [2], its length is 1.
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = FindLengthOfLCIS(new[] { 1, 3, 5, 4, 7 });
+            var result2 = FindLengthOfLCIS(new[] { 2, 2, 2, 2, 2 });
+        }
+
+        public static int FindLengthOfLCIS(int[] nums)
+        {
+            var result = 0;
+            var count = 0;
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                //if is first index or after greater than before
+                if (i == 0 || nums[i - 1] < nums[i])
+                {
+                    result = Math.Max(result, ++count);
+                }
+                else
+                {
+                    count = 1;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ FindShortestSubArray/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindShortestSubArray
+{
+    class Program
+    {
+        /*
+         * Given a non-empty array of non-negative integers nums, the degree of this array is defined as the maximum frequency of any one of its elements.
+         * Your task is to find the smallest possible length of a (contiguous) subarray of nums, that has the same degree as nums.
+         *
+         * Example 1:
+         * Input: [1, 2, 2, 3, 1]
+         * Output: 2
+         *
+         * Explanation:
+         * The input array has a degree of 2 because both elements 1 and 2 appear twice. Of the subarrays that have the same degree:
+         * [1, 2, 2, 3, 1], [1, 2, 2, 3], [2, 2, 3, 1], [1, 2, 2], [2, 2, 3], [2, 2]
+         * The shortest length is 2. So return 2.
+         *
+         * Example 2:
+         * Input: [1,2,2,3,1,4,2]
+         * Output: 6
+         *
+         * Note:
+         * nums.length will be between 1 and 50,000.
+         * nums[i] will be an integer between 0 and 49,999.
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = FindShortestSubArray(new[] { 1 });
+            var result2 = FindShortestSubArray(new[] { 1, 2, 2, 3, 1, 4, 2 });
+        }
+
+        public static int FindShortestSubArray(int[] nums)
+        {
+            var counter = new Dictionary<int, int>();
+            var first = new Dictionary<int, int>();
+            var shortestLength = 0;
+            var degree = 0;
+
+            for (var i = 0; i < nums.Length; ++i)
+            {
+                //store each number and index when first found
+                if (!first.ContainsKey(nums[i]))
+                {
+                    first.Add(nums[i], i);
+                }
+
+                //store counter for each number
+                if (counter.ContainsKey(nums[i]))
+                {
+                    counter[nums[i]] += 1;
+                }
+                else
+                {
+                    counter.Add(nums[i], 1);
+                }
+
+                //if any number exists more than 1
+                if (counter[nums[i]] > degree)
+                {
+                    //set the number count to degree
+                    degree = counter[nums[i]];
+                    //get latest index when greater number count is found
+                    shortestLength = i - first[nums[i]] + 1;
+                }
+                else if (counter[nums[i]] == degree)
+                {
+                    shortestLength = Math.Min(shortestLength, i - first[nums[i]] + 1);
+                }
+            }
+
+            return shortestLength;
+        }
+    }
+}
