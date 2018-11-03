d18854f0849fc329a3dc648d13e0614fb7a49db2
Hon Loon <tenghonloon@hotmail.com>
2018-11-03 22:21:51 +0800

    Reverse & TwoSum2

---
+++ Reverse/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * 7. Reverse Integer
+ * https://leetcode.com/problems/reverse-integer/
+ */
+namespace Reverse
+{
+    /*
+     * Given a 32-bit signed integer, reverse digits of an integer.
+     *
+     * Example 1:
+     * Input: 123
+     * Output: 321
+     *
+     * Example 2:
+     * Input: -123
+     * Output: -321
+     *
+     * Example 3:
+     * Input: 120
+     * Output: 21
+     *
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Reverse(123);
+            var result2 = Reverse(-123);
+            var result3 = Reverse(120);
+        }
+
+        public static int Reverse(int x)
+        {
+            var result = 0;
+
+            while (x != 0)
+            {
+                var tail = x % 10;
+                var newResult = result * 10 + tail;
+
+                //when overflow return 0, e.g. if the value is 123467899
+                if ((newResult - tail) / 10 != result)
+                {
+                    return 0;
+                }
+
+                result = newResult;
+                x = x / 10;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TwoSum2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+/*
+ * 1. Two Sum
+ * https://leetcode.com/problems/two-sum/
+ */
+
+namespace TwoSum2
+{
+    /*
+     * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+     * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+     *
+     * Example:
+     * Given nums = [2, 7, 11, 15], target = 9,
+     * Because nums[0] + nums[1] = 2 + 7 = 9,
+     * return [0, 1].
+     *
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = TwoSum(new[] {2, 7, 11, 15}, 9);
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var dictionary = new Dictionary<int, int>();
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var numberToFind = target - nums[i];
+
+                if (!dictionary.ContainsKey(numberToFind))
+                {
+                    dictionary[nums[i]] = i;
+                }
+                else
+                {
+                    return new[] {dictionary[numberToFind], i};
+                }
+            }
+
+            return null;
+        }
+    }
+}
