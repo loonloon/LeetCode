bcb92d1769204cf376f82eb8240b9d4542fcd56f
Hon Loon <tenghonloon@hotmail.com>
2019-01-02 22:12:54 +0800

    MaxSubArray & LevelOrderBottom

---
+++ LevelOrderBottom/Program.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+/*
+ * 107. Binary Tree Level Order Traversal II
+ * https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
+ */
+namespace LevelOrderBottom
+{
+    class Program
+    {
+        /*
+         * Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+         *
+         * For example:
+         * Given binary tree [3,9,20,null,null,15,7],
+         *    3
+         *   / \
+         *  9  20
+         *    /  \
+         *   15   7
+         *
+         * return its bottom-up level order traversal as:
+         * [
+         *  [15,7],
+         *  [9,20],
+         *  [3]
+         * ]
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = LevelOrderBottom(new TreeNode(3)
+            {
+                Left = new TreeNode(9),
+                Right = new TreeNode(20)
+                {
+                    Left = new TreeNode(15),
+                    Right = new TreeNode(7)
+                }
+            });
+        }
+
+        public static IList<IList<int>> LevelOrderBottom(TreeNode root)
+        {
+            var queue = new Queue<TreeNode>();
+            var wrapList = new List<IList<int>>();
+
+            if (root == null) 
+            {
+                return wrapList;
+            }
+
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                var levelNum = queue.Count;
+                var subList = new List<int>();
+
+                for (var i = 0; i < levelNum; i++)
+                {
+                    if (queue.Peek().Left != null)
+                    {
+                        queue.Enqueue(queue.Peek().Left);
+                    }
+
+                    if (queue.Peek().Right != null)
+                    {
+                        queue.Enqueue(queue.Peek().Right);
+                    }
+
+                    subList.Add(queue.Dequeue().Val);
+                }
+
+                wrapList.Insert(0, subList);
+            }
+
+            return wrapList;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int Val;
+        public TreeNode Left;
+        public TreeNode Right;
+
+        public TreeNode(int x)
+        {
+            Val = x;
+        }
+    }
+}
---
+++ MaxSubArray/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace MaxSubArray
+{
+    /*
+     * 53. Maximum Subarray
+     * https://leetcode.com/problems/maximum-subarray/
+     */
+    class Program
+    {
+        /*
+         * Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+         * Example:
+         * Input: [-2,1,-3,4,-1,2,1,-5,4],
+         * Output: 6
+         * Explanation: [4,-1,2,1] has the largest sum = 6.
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = MaxSubArray(new[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 });
+        }
+
+        public static int MaxSubArray(int[] nums)
+        {
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+
+            //Kadane's Algorithm
+            var max = nums[0];
+
+            for (var i = 1; i < nums.Length; i++)
+            {
+                var next = nums[i] + nums[i - 1];
+
+                if (nums[i] < next)
+                {
+                    nums[i] = next;
+                }
+
+                if (max < nums[i])
+                {
+                    max = nums[i];
+                }
+            }
+
+            return max;
+        }
+    }
+}
