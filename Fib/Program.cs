4d10e7751faaa85499589f34f93f977fdc98d707
loonloon <tenghonloon@hotmail.com>
2019-05-15 16:49:54 +0800

    Fib & IsUnivalTree

---
+++ Fib/Program.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * 509. Fibonacci Number
+ * https://leetcode.com/problems/fibonacci-number/
+ */
+namespace Fib
+{
+    class Program
+    {
+        /*
+         * The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence,
+         * such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+         *
+         * F(0) = 0,   F(1) = 1
+         * F(N) = F(N - 1) + F(N - 2), for N > 1.
+         *
+         * Given N, calculate F(N).
+         *
+         * Example 1:
+         * Input: 2
+         * Output: 1
+         * Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+         *
+         * Example 2:
+         * Input: 3
+         * Output: 2
+         * Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+         *
+         * Example 3:
+         * Input: 4
+         * Output: 3
+         * Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+         *
+         * Note:
+         * 0 ≤ N ≤ 30.
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = Fib(2);
+            var result2 = Fib(3);
+            var result3 = Fib(4);
+        }
+
+        public static int Fib(int N)
+        {
+            //Iterative
+            //if (N <= 1)
+            //{
+            //    return N;
+            //}
+
+            //int a = 0, b = 1;
+
+            //while (N-- > 1)
+            //{
+            //    var sum = a + b;
+            //    a = b;
+            //    b = sum;
+            //}
+
+            //return b;
+
+            return N <= 1 ? N : Fib(N - 1) + Fib(N - 2);
+        }
+    }
+}
---
+++ IsUnivalTree/Program.cs
@@ -0,0 +1,110 @@
+﻿/*
+ * 965. Univalued Binary Tree
+ * https://leetcode.com/problems/univalued-binary-tree/
+ */
+
+using System.Collections.Generic;
+
+namespace IsUnivalTree
+{
+    class Program
+    {
+        /*
+         * A binary tree is univalued if every node in the tree has the same value.
+         * Return true if and only if the given tree is univalued.
+         *
+         * Example 1:
+         *
+         *     1
+         *    / \
+         *   1   1 
+         *  / \   \
+         * 1   1   1
+         *
+         * Input: [1,1,1,1,1,null,1]
+         * Output: true
+         *
+         * Example 2:
+         *
+         *     2
+         *    / \
+         *   2   2 
+         *  / \  
+         * 5   2   
+         *
+         * Input: [2,2,2,5,2]
+         * Output: false
+         *
+         * Note:
+         * The number of nodes in the given tree will be in the range [1, 100].
+         * Each node's value will be an integer in the range [0, 99].
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = IsUnivalTree(new TreeNode(1)
+            {
+                Left = new TreeNode(1)
+                {
+                    Left = new TreeNode(1),
+                    Right = new TreeNode(1)
+                },
+                Right = new TreeNode(1)
+                {
+                    Right = new TreeNode(1)
+                }
+            });
+
+            var result2 = IsUnivalTree(new TreeNode(2)
+            {
+                Left = new TreeNode(2)
+                {
+                    Left = new TreeNode(5),
+                    Right = new TreeNode(2)
+                },
+                Right = new TreeNode(2)
+            });
+        }
+
+        public static bool IsUnivalTree(TreeNode root)
+        {
+            var stack = new Stack<TreeNode>();
+            stack.Push(root);
+
+            while (stack.Count > 0)
+            {
+                var currentNode = stack.Pop();
+
+                if (currentNode.Left != null && currentNode.Left.Val != currentNode.Val ||
+                    currentNode.Right != null && currentNode.Right.Val != currentNode.Val)
+                {
+                    return false;
+                }
+
+                if (currentNode.Left != null)
+                {
+                    stack.Push(currentNode.Left);
+                }
+
+                if (currentNode.Right != null)
+                {
+                    stack.Push(currentNode.Right);
+                }
+            }
+
+            return true;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int Val;
+        public TreeNode Left;
+        public TreeNode Right;
+
+        public TreeNode(int x)
+        {
+            Val = x;
+        }
+    }
+}
