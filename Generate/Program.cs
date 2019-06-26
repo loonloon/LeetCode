1caf4cd6f97e2d1a4e5055dc49fe8525a16ecac1
40204649 <40204649@CPC402-4649>
2019-06-26 16:23:02 +0800

    Generate & GetRow

---
+++ Generate/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+/*
+ * 118. Pascal's Triangle
+ * https://leetcode.com/problems/pascals-triangle/
+ */
+namespace Generate
+{
+    /*
+     * Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+     *
+     * Example:
+     * Input: 5
+     *
+     * Output:
+     * [
+     *     [1],
+     *    [1,1],
+     *   [1,2,1],
+     *  [1,3,3,1],
+     * [1,4,6,4,1]
+     * ]
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Generate(5);
+        }
+
+        public static IList<IList<int>> Generate(int numRows)
+        {
+            var rows = new List<IList<int>>();
+
+            for (var i = 0; i < numRows; i++)
+            {
+                var row = new List<int>();
+
+                for (var j = 0; j < i + 1; j++)
+                {
+                    // if is first or last
+                    if (j == 0 || j == i)
+                    {
+                        row.Add(1);
+                    }
+                    else
+                    {
+                        // access previous row and the cells value
+                        var a = rows[i - 1][j - 1];
+                        var b = rows[i - 1][j];
+                        row.Add(a + b);
+                    }
+                }
+
+                rows.Add(row);
+            }
+
+            return rows;
+        }
+    }
+}
---
+++ GetRow/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+/*
+ * 119. Pascal's Triangle II
+ * https://leetcode.com/problems/pascals-triangle-ii/
+ */
+namespace GetRow
+{
+    class Program
+    {
+        /*
+         * Given a non-negative index k where k ≤ 33, return the kth index row of the Pascal's triangle.
+         * Note that the row index starts from 0.
+         *
+         * Example:
+         * Input: 3
+         * Output: [1,3,3,1]
+         *
+         * Follow up:
+         * Could you optimize your algorithm to use only O(k) extra space?
+         */
+        static void Main(string[] args)
+        {
+            var result = GetRow(3);
+        }
+
+        public static IList<int> GetRow(int rowIndex)
+        {
+            var row = new List<int> { 1 };
+
+            // set row values to 0 from index 1
+            for (var i = 1; i < rowIndex + 1; i++)
+            {
+                row.Add(0);
+            }
+
+            // each row
+            for (var i = 1; i < rowIndex + 1; i++)
+            {
+                // each row content
+                for (var j = i; j >= 1; j--)
+                {
+                    row[j] += row[j - 1];
+                }
+            }
+
+            return row;
+        }
+    }
+}
