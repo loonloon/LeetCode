2e0d85fc423ecdd939b5351de27f92c845facef8
40204649 <40204649@CPC402-4649>
2019-10-03 17:11:32 +0800

    DefangIPaddr & MinDeletionSize & UniqueOccurrences

---
+++ DefangIPaddr/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * 1108. Defanging an IP Address
+ * https://leetcode.com/problems/defanging-an-ip-address/
+ */
+namespace DefangIPaddr
+{
+    class Program
+    {
+        /*
+         * Given a valid (IPv4) IP address, return a defanged version of that IP address.
+         * A defanged IP address replaces every period "." with "[.]".
+         *
+         * Example 1:
+         * Input: address = "1.1.1.1"
+         * Output: "1[.]1[.]1[.]1"
+         *
+         * Example 2:
+         * Input: address = "255.100.50.0"
+         * Output: "255[.]100[.]50[.]0"
+         *
+         * Constraints:
+         * The given address is a valid IPv4 address.
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = DefangIPaddr("1.1.1.1");
+            var result2 = DefangIPaddr("255.100.50.0");
+        }
+
+        public static string DefangIPaddr(string address)
+        {
+            return address.Replace(".", "[.]");
+        }
+    }
+}
---
+++ UniqueOccurrences/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+
+/*
+ * 1207. Unique Number of Occurrences
+ * https://leetcode.com/problems/unique-number-of-occurrences/
+ */
+namespace UniqueOccurrences
+{
+    class Program
+    {
+        /*
+         * Given an array of integers arr, write a function that returns true if and only
+         * if the number of occurrences of each value in the array is unique.
+         *
+         * Example 1:
+         * Input: arr = [1,2,2,1,1,3]
+         * Output: true
+         * Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+         *
+         * Example 2:
+         * Input: arr = [1,2]
+         * Output: false
+         *
+         * Example 3:
+         * Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+         * Output: true
+         *
+         * Constraints:
+         * 1 <= arr.length <= 1000
+         * -1000 <= arr[i] <= 1000
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = UniqueOccurrences(new[] { 1, 2, 2, 1, 1, 3 });
+            var result2 = UniqueOccurrences(new[] { 1, 2 });
+            var result3 = UniqueOccurrences(new[] { -3, 0, 1, -3, 1, 1, 1, -3, 10, 0 });
+        }
+
+        public static bool UniqueOccurrences(int[] arr)
+        {
+            var dictionary = new Dictionary<int, int>();
+
+            for (var i = 0; i < arr.Length; i++)
+            {
+                var value = arr[i];
+
+                if (!dictionary.ContainsKey(value))
+                {
+                    dictionary.Add(value, 1);
+                }
+                else
+                {
+                    dictionary[value] += 1;
+                }
+            }
+
+            return dictionary.Values.Count == new HashSet<int>(dictionary.Values).Count;
+        }
+    }
+}
