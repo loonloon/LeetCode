4bdb4eeb00c53b35ebf7c099fbd3a724b608792c
40204649 <40204649@CPC402-4649>
2019-10-11 16:25:08 +0800

    IsLongPressedName & ReorderLogFiles

---
+++ IsLongPressedName/Program.cs
@@ -0,0 +1,72 @@
+﻿/*
+ * 925. Long Pressed Name
+ * https://leetcode.com/problems/long-pressed-name/
+ */
+namespace IsLongPressedName
+{
+    class Program
+    {
+        /*
+         * Your friend is typing his name into a keyboard.  Sometimes, when typing a character c,
+         * the key might get long pressed, and the character will be typed 1 or more times.
+         *
+         * You examine the typed characters of the keyboard.  Return True if it is possible that it was your friends name,
+         * with some characters (possibly none) being long pressed.
+         *
+         * Example 1:
+         * Input: name = "alex", typed = "aaleex"
+         * Output: true
+         * Explanation: 'a' and 'e' in 'alex' were long pressed.
+         *
+         * Example 2:
+         * Input: name = "saeed", typed = "ssaaedd"
+         * Output: false
+         * Explanation: 'e' must have been pressed twice, but it wasn't in the typed output.
+         *
+         * Example 3:
+         * Input: name = "leelee", typed = "lleeelee"
+         * Output: true
+         *
+         * Example 4:
+         * Input: name = "laiden", typed = "laiden"
+         * Output: true
+         * Explanation: It's not necessary to long press any character.
+         *
+         * Note:
+         * name.length <= 1000
+         * typed.length <= 1000
+         * The characters of name and typed are lowercase letters.
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = IsLongPressedName("alex", "aaleex");
+            var result2 = IsLongPressedName("saeed", "ssaaedd");
+            var result3 = IsLongPressedName("leelee", "lleeelee");
+            var result4 = IsLongPressedName("laiden", "laiden");
+        }
+
+        public static bool IsLongPressedName(string name, string typed)
+        {
+            var nameIndex = 0;
+
+            for (var i = 0; i < typed.Length; i++)
+            {
+                if (nameIndex < name.Length && name[nameIndex] == typed[i])
+                {
+                    nameIndex++;
+                }
+                else if (i > 0 && typed[i] == typed[i - 1])
+                {
+                    //do nothing if current character is same as previous character
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return nameIndex == name.Length;
+        }
+    }
+}
---
+++ ReorderLogFiles/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+/*
+ * 937. Reorder Data in Log Files
+ * https://leetcode.com/problems/reorder-data-in-log-files/
+ */
+namespace ReorderLogFiles
+{
+    class Program
+    {
+        /*
+         * You have an array of logs.  Each log is a space delimited string of words.
+         * For each log, the first word in each log is an alphanumeric identifier.  Then, either:
+         *
+         * Each word after the identifier will consist only of lowercase letters, or;
+         * Each word after the identifier will consist only of digits.
+         *
+         * We will call these two varieties of logs letter-logs and digit-logs.
+         * It is guaranteed that each log has at least one word after its identifier.
+         *
+         * Reorder the logs so that all of the letter-logs come before any digit-log.
+         * The letter-logs are ordered lexicographically ignoring identifier, with the identifier used in case of ties.
+         * The digit-logs should be put in their original order.
+         * Return the final order of the logs.
+         *
+         * Example 1:
+         * Input: logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
+         * Output: ["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5 1","dig2 3 6"]
+         *
+         * Constraints:
+         * 0 <= logs.length <= 100
+         * 3 <= logs[i].length <= 100
+         * logs[i] is guaranteed to have an identifier, and a word after the identifier.
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = ReorderLogFiles(new[] { "dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero" });
+        }
+
+        public static string[] ReorderLogFiles(string[] logs)
+        {
+            var letterList = new List<string>();
+            var digitList = new List<string>();
+
+            for (var i = 0; i < logs.Length; i++)
+            {
+                var spaceIndex = logs[i].IndexOf(' ');
+                var c = logs[i][spaceIndex + 1];
+
+                if (c - '0' <= 9)
+                {
+                    digitList.Add(logs[i]);
+                }
+                else
+                {
+                    letterList.Add(logs[i]);
+                }
+            }
+
+            letterList.Sort((s1, s2) =>
+            {
+                var spaceIndex1 = s1.IndexOf(' ');
+                var spaceIndex2 = s2.IndexOf(' ');
+
+                var letter1 = s1.Substring(spaceIndex1);
+                var letter2 = s2.Substring(spaceIndex2);
+                var letterResult = letter1.CompareTo(letter2);
+
+                if (letterResult != 0)
+                {
+                    return letterResult;
+                }
+
+                var id1 = s1.Substring(0, spaceIndex1);
+                var id2 = s2.Substring(0, spaceIndex2);
+
+                return id1.CompareTo(id2);
+            });
+
+            var result = new string[logs.Length];
+
+            for (var i = 0; i < letterList.Count; i++)
+            {
+                result[i] = letterList[i];
+            }
+
+            for (var i = 0; i < digitList.Count; i++)
+            {
+                result[i + letterList.Count] = digitList[i];
+            }
+
+            return result;
+        }
+    }
+}
