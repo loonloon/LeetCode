b956eba8b053362916d2dc8c3ed0a2c84f4d8b1d
40204649 <40204649@CPC402-4649>
2019-10-24 16:56:00 +0800

    ContainsNearbyDuplicate & MyStackUsingQueue

---
+++ ContainsNearbyDuplicate/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+/*
+ * 219. Contains Duplicate II
+ * https://leetcode.com/problems/contains-duplicate-ii/
+ */
+namespace ContainsNearbyDuplicate
+{
+    class Program
+    {
+        /*
+         * Given an array of integers and an integer k, find out whether there are two distinct indices i and j
+         * in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
+         *
+         * Example 1:
+         * Input: nums = [1,2,3,1], k = 3
+         * Output: true
+         *
+         * Example 2:
+         * Input: nums = [1,0,1,1], k = 1
+         * Output: true
+         *
+         * Example 3:
+         * Input: nums = [1,2,3,1,2,3], k = 2
+         * Output: false
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = ContainsNearbyDuplicate(new[] { 1, 2, 3, 1 }, 3);
+            var result2 = ContainsNearbyDuplicate(new[] { 1, 0, 1, 1 }, 1);
+            var result3 = ContainsNearbyDuplicate(new[] { 1, 2, 3, 1, 2, 3 }, 2);
+        }
+
+        public static bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            var hashSet = new HashSet<int>();
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (hashSet.Contains(nums[i]))
+                {
+                    return true;
+                }
+
+                hashSet.Add(nums[i]);
+
+                if (hashSet.Count > k)
+                {
+                    //remove old entry
+                    hashSet.Remove(nums[Math.Max(i - k, 0)]);
+                }
+            }
+
+            return false;
+
+            //for (var i = 0; i < nums.Length; i++)
+            //{
+            //    for (var j = i + 1; j < nums.Length; j++)
+            //    {
+            //        if ((nums[i] == nums[j]) && ((j - i) <= k))
+            //        {
+            //            return true;
+            //        }
+            //    }
+            //}
+
+            //return false;
+        }
+    }
+}
---
+++ MyStackUsingQueue/Program.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+/*
+ * 225. Implement Stack using Queues
+ * https://leetcode.com/problems/implement-stack-using-queues/
+ */
+namespace MyStackUsingQueue
+{
+    class Program
+    {
+        /*
+         * Implement the following operations of a stack using queues.
+         * push(x) -- Push element x onto stack.
+         * pop() -- Removes the element on top of the stack.
+         * top() -- Get the top element.
+         * empty() -- Return whether the stack is empty.
+         *
+         * Example:
+         * MyStack stack = new MyStack();
+         * stack.push(1);
+         * stack.push(2);
+         * stack.top();   // returns 2
+         * stack.pop();   // returns 2
+         * stack.empty(); // returns false
+         *
+         * Notes:
+         * You must use only standard operations of a queue -- which means only push to back, peek/pop from front, size,
+         * and is empty operations are valid.
+         *
+         * Depending on your language, queue may not be supported natively.
+         * You may simulate a queue by using a list or deque (double-ended queue), as long as you use only standard operations of a queue.
+         *
+         * You may assume that all operations are valid (for example, no pop or top operations will be called on an empty stack).
+         *
+         */
+        static void Main(string[] args)
+        {
+            var obj = new MyStack();
+            obj.Push(1);
+            obj.Push(2);
+
+            var param2 = obj.Top();
+            var param3 = obj.Pop();
+            var param4 = obj.Empty();
+        }
+    }
+
+    public class MyStack
+    {
+        private readonly Queue<int> _queueFirst;
+        private readonly Queue<int> _queueSecond;
+
+        /** Initialize your data structure here. */
+        public MyStack()
+        {
+            _queueFirst = new Queue<int>();
+            _queueSecond = new Queue<int>();
+        }
+
+        /** Push element x onto stack. */
+        public void Push(int x)
+        {
+            while (_queueFirst.Count > 0)
+            {
+                _queueSecond.Enqueue(_queueFirst.Dequeue());
+            }
+
+            _queueFirst.Enqueue(x);
+
+            while (_queueSecond.Count > 0)
+            {
+                _queueFirst.Enqueue(_queueSecond.Dequeue());
+            }
+        }
+
+        /** Removes the element on top of the stack and returns that element. */
+        public int Pop()
+        {
+            return _queueFirst.Dequeue();
+        }
+
+        /** Get the top element. */
+        public int Top()
+        {
+            return _queueFirst.Peek();
+        }
+
+        /** Returns whether the stack is empty. */
+        public bool Empty()
+        {
+            return _queueFirst.Count == 0;
+        }
+    }
+
+}
