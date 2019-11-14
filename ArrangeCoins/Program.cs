a69f48963859d3558f69a03de392fe14b408f907
40204649 <40204649@CPC402-4649>
2019-11-14 17:45:04 +0800

    ArrangeCoins & RepeatedSubstringPattern

---
+++ ArrangeCoins/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * 441. Arranging Coins
+ * https://leetcode.com/problems/arranging-coins/
+ */
+namespace ArrangeCoins
+{
+    class Program
+    {
+        /*
+         * You have a total of n coins that you want to form in a staircase shape, where every k-th row must have exactly k coins.
+         * Given n, find the total number of full staircase rows that can be formed.
+         * n is a non-negative integer and fits within the range of a 32-bit signed integer.
+         *
+         * Example 1:
+         * n = 5
+         * The coins can form the following rows:
+         * ¤
+         * ¤ ¤
+         * ¤ ¤
+         * Because the 3rd row is incomplete, we return 2.
+         *
+         * Example 2:
+         * n = 8
+         * The coins can form the following rows:
+         * ¤
+         * ¤ ¤
+         * ¤ ¤ ¤
+         * ¤ ¤
+         * Because the 4th row is incomplete, we return 3.
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = ArrangeCoins(5);
+            var result2 = ArrangeCoins(8);
+        }
+
+        public static int ArrangeCoins(int n)
+        {
+            var i = 1;
+
+            while (n >= i)
+            {
+                n -= i;
+                i++;
+            }
+
+            return i - 1;
+        }
+    }
+}
---
+++ RepeatedSubstringPattern/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+/*
+ * 459. Repeated Substring Pattern
+ * https://leetcode.com/problems/repeated-substring-pattern/
+ */
+namespace RepeatedSubstringPattern
+{
+    class Program
+    {
+        /*
+         * Given a non-empty string check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.
+         * You may assume the given string consists of lowercase English letters only and its length will not exceed 10000.
+         *
+         * Example 1:
+         * Input: "abab"
+         * Output: True
+         * Explanation: It's the substring "ab" twice.
+         *
+         * Example 2:
+         * Input: "aba"
+         * Output: False
+         *
+         * Example 3:
+         * Input: "abcabcabcabc"
+         * Output: True
+         * Explanation: It's the substring "abc" four times. (And the substring "abcabc" twice.)
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = RepeatedSubstringPattern("abab");
+            var result2 = RepeatedSubstringPattern("aba");
+            var result3 = RepeatedSubstringPattern("abcabcabcabc");
+        }
+
+        public static bool RepeatedSubstringPattern(string s)
+        {
+            //why divider is 2? if the repeated string always even number
+            var divider = 2;
+
+            while (divider <= s.Length)
+            {
+                if (s.Length % divider == 0)
+                {
+                    var chunk = s.Substring(0, s.Length / divider);
+                    var result = new StringBuilder();
+
+                    while (result.Length != s.Length)
+                    {
+                        result.Append(chunk);
+                    }
+
+                    if (result.ToString() == s)
+                    {
+                        return true;
+                    }
+                }
+
+                divider++;
+            }
+
+            return false;
+        }
+    }
+}
