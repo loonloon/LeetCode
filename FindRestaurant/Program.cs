086e6be9bb81a0e0e3b9e780ae150c18509214cf
Hon Loon <tenghonloon@hotmail.com>
2020-01-12 11:20:32 +0800

    FindRestaurant & FindUnsortedSubarray

---
+++ FindRestaurant/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+/*
+ * 599. Minimum Index Sum of Two Lists
+ * https://leetcode.com/problems/minimum-index-sum-of-two-lists/
+ */
+namespace FindRestaurant
+{
+    class Program
+    {
+        /*
+         * Suppose Andy and Doris want to choose a restaurant for dinner, and they both have a list of favorite restaurants represented by strings.
+         * You need to help them find out their common interest with the least list index sum.
+         * If there is a choice tie between answers, output all of them with no order requirement. You could assume there always exists an answer.
+         *
+         * Example 1:
+         * Input:
+         * ["Shogun", "Tapioca Express", "Burger King", "KFC"]
+         * ["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]
+         * Output: ["Shogun"]
+         * Explanation: The only restaurant they both like is "Shogun".
+         *
+         * Example 2:
+         * Input:
+         * ["Shogun", "Tapioca Express", "Burger King", "KFC"]
+         * ["KFC", "Shogun", "Burger King"]
+         * Output: ["Shogun"]
+         *
+         * Explanation: The restaurant they both like and have the least index sum is "Shogun" with index sum 1 (0+1).
+         * Note:
+         * The length of both lists will be in the range of [1, 1000].
+         * The length of strings in both lists will be in the range of [1, 30].
+         * The index is starting from 0 to the list length minus 1.
+         * No duplicates in both lists.
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = FindRestaurant(new[] { "Shogun", "Tapioca Express", "Burger King", "KFC" }, new[] { "Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun" });
+            var result2 = FindRestaurant(new[] { "Shogun", "Tapioca Express", "Burger King", "KFC" }, new[] { "KFC", "Shogun", "Burger King" });
+        }
+
+        public static string[] FindRestaurant(string[] list1, string[] list2)
+        {
+            var map = new Dictionary<string, int>();
+
+            for (var i = 0; i < list1.Length; i++)
+            {
+                map.Add(list1[i], i);
+            }
+
+            var minSumIndex = int.MaxValue;
+            var result = new List<string>();
+
+            for (var i = 0; i < list2.Length; i++)
+            {
+                if (!map.ContainsKey(list2[i]))
+                {
+                    continue;
+                }
+
+                var list1Index = map[list2[i]];
+                var sumIndex = i + list1Index;
+
+                if (sumIndex > minSumIndex)
+                {
+                    continue;
+                }
+
+                if (sumIndex < minSumIndex)
+                {
+                    result.Clear();
+                    minSumIndex = sumIndex;
+                }
+
+                result.Add(list2[i]);
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ FindUnsortedSubarray/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+/*
+ * 581. Shortest Unsorted Continuous Subarray
+ * https://leetcode.com/problems/shortest-unsorted-continuous-subarray/
+ */
+namespace FindUnsortedSubarray
+{
+    class Program
+    {
+        /*
+         * Given an integer array, you need to find one continuous subarray that if you only sort this subarray in ascending order, then the whole array will be sorted in ascending order, too.
+         * You need to find the shortest such subarray and output its length.
+         *
+         * Example 1:
+         * Input: [2, 6, 4, 8, 10, 9, 15]
+         * Output: 5
+         * Explanation: You need to sort [6, 4, 8, 10, 9] in ascending order to make the whole array sorted in ascending order.
+         *
+         * Note:
+         * Then length of the input array is in range [1, 10,000].
+         * The input array may contain duplicates, so ascending order here means <=.
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = FindUnsortedSubarray(new[] { 2, 6, 4, 8, 10, 9, 15 });
+        }
+
+        public static int FindUnsortedSubarray(int[] nums)
+        {
+            var n = nums.Length;
+            //if the nums is sorted, then it can become zero
+            var head = -1;
+            var tail = -2;
+            var min = nums[n - 1];
+            var max = nums[0];
+
+            for (var i = 1; i < n; i++)
+            {
+                var tailIndex = n - 1 - i;
+                min = Math.Min(min, nums[tailIndex]);
+                max = Math.Max(max, nums[i]);
+
+                if (nums[i] < max)
+                {
+                    tail = i;
+                }
+
+                if (nums[tailIndex] > min)
+                {
+                    head = tailIndex;
+                }
+            }
+
+            return tail - head + 1;
+
+            //var n = nums.Length;
+            //var temp = new int[n];
+
+            //for (var i = 0; i < n; i++)
+            //{
+            //    temp[i] = nums[i];
+            //}
+
+            //Array.Sort(temp);
+
+            //var start = -1;
+            //var end = -1;
+
+            //for (var i = 0; i < n; i++)
+            //{
+            //    if (nums[i] == temp[i])
+            //    {
+            //        continue;
+            //    }
+
+            //    if (start == -1)
+            //    {
+            //        start = i;
+            //    }
+
+            //    end = i;
+            //}
+
+            //var count = end - start;
+            //return count == 0 ? count : count + 1;
+        }
+    }
+}
