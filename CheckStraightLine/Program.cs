7366d1f7c1d815694e286246216b86d0cab1264b
Hon Loon <tenghonloon@hotmail.com>
2020-03-24 21:17:14 +0800

    CheckStraightLine & ValidPalindrome

---
+++ CheckStraightLine/Program.cs
@@ -0,0 +1,75 @@
+﻿/*
+ * 1232. Check If It Is a Straight Line
+ * https://leetcode.com/problems/check-if-it-is-a-straight-line/
+ */
+namespace CheckStraightLine
+{
+    class Program
+    {
+        /*
+         * You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point.
+         * Check if these points make a straight line in the XY plane.
+         *
+         * Example 1:
+         * Input: coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
+         * Output: true
+         *
+         * Example 2:
+         * Input: coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
+         * Output: false
+         *
+         * Constraints:
+         * 2 <= coordinates.length <= 1000
+         * coordinates[i].length == 2
+         * -10^4 <= coordinates[i][0], coordinates[i][1] <= 10^4
+         * coordinates contains no duplicate point.
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = CheckStraightLine(new[]
+            {
+                new []{ 1, 2 },
+                new []{ 2, 3 },
+                new []{ 3, 4 },
+                new []{ 4, 5 },
+                new []{ 5, 6 },
+                new []{ 6, 7 }
+            });
+
+            var result2 = CheckStraightLine(new[]
+            {
+                new []{ 1, 1 },
+                new []{ 2, 2 },
+                new []{ 3, 4 },
+                new []{ 4, 5 },
+                new []{ 5, 6 },
+                new []{ 7, 7 }
+            });
+        }
+
+        public static bool CheckStraightLine(int[][] coordinates)
+        {
+            var x0 = coordinates[0][0];
+            var y0 = coordinates[0][1];
+            var x1 = coordinates[1][0];
+            var y1 = coordinates[1][1];
+
+            var dx = x1 - x0;
+            var dy = y1 - y0;
+
+            foreach (var co in coordinates)
+            {
+                var x = co[0];
+                var y = co[1];
+
+                if (dx * (y - y1) != dy * (x - x1))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ ValidPalindrome/Program.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * 680. Valid Palindrome II
+ * https://leetcode.com/problems/valid-palindrome-ii/
+ */
+namespace ValidPalindrome
+{
+    class Program
+    {
+        /*
+         * Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
+         *
+         * Example 1:
+         * Input: "aba"
+         * Output: True
+         *
+         * Example 2:
+         * Input: "abca"
+         * Output: True
+         *
+         * Explanation: You could delete the character 'c'.
+         * Note:
+         * The string will only contain lowercase characters a-z. The maximum length of the string is 50000.
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = ValidPalindrome("aba");
+            var result2 = ValidPalindrome("abca");
+        }
+
+        public static bool ValidPalindrome(string s)
+        {
+            for (int i = 0, j = s.Length - 1; i < j; i++, j--)
+            {
+                if (s[i] == s[j])
+                {
+                    continue;
+                }
+
+                var leftIndex1 = i;
+                var rightIndex1 = j - 1;
+                var leftIndex2 = i + 1;
+                var rightIndex2 = j;
+
+                //removing left
+                while (leftIndex1 < rightIndex1 && s[leftIndex1] == s[rightIndex1])
+                {
+                    leftIndex1++;
+                    rightIndex1--;
+                }
+
+                //removing right
+                while (leftIndex2 < rightIndex2 && s[leftIndex2] == s[rightIndex2])
+                {
+                    leftIndex2++;
+                    rightIndex2--;
+                }
+
+                return leftIndex1 >= rightIndex1 || leftIndex2 >= rightIndex2;
+            }
+
+            return true;
+        }
+    }
+}
