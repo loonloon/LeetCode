70b71b1558ffb19eb6c26de6a098eb38adca8fd8
Teng Hon Loon <tenghonloon@hotmail.com>
2020-11-20 16:17:40 +0800

    CheckPerfectNumber& FindRelativeRanks

---
+++ CheckPerfectNumber/Program.cs
@@ -0,0 +1,74 @@
+﻿/*
+ * 507. Perfect Number
+ * https://leetcode.com/problems/perfect-number/
+ */
+
+using System;
+
+namespace CheckPerfectNumber
+{
+    class Program
+    {
+        /*
+         * A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself.
+         * A divisor of an integer x is an integer that can divide x evenly.
+         *
+         * Given an integer n, return true if n is a perfect number, otherwise return false.
+         * Example 1:
+         * Input: num = 28
+         * Output: true
+         * Explanation: 28 = 1 + 2 + 4 + 7 + 14
+         * 1, 2, 4, 7, and 14 are all divisors of 28.
+         *
+         * Example 2:
+         * Input: num = 6
+         * Output: true
+         *
+         * Example 3:
+         * Input: num = 496
+         * Output: true
+         *
+         * Example 4:
+         * Input: num = 8128
+         * Output: true
+         *
+         * Example 5:
+         * Input: num = 2
+         * Output: false
+         *
+         * Constraints:
+         * 1 <= num <= 108
+         *
+         */
+        static void Main(string[] args)
+        {
+            var result = CheckPerfectNumber(28);
+            var result2 = CheckPerfectNumber(6);
+            var result3 = CheckPerfectNumber(496);
+            var result4 = CheckPerfectNumber(8128);
+            var result5 = CheckPerfectNumber(2);
+        }
+
+        public static bool CheckPerfectNumber(int num)
+        {
+            if (num == 1)
+            {
+                return false;
+            }
+
+            var sum = 0;
+            var sqrt = Math.Sqrt(num);
+
+            for (var i = 2; i <= sqrt; i++)
+            {
+                if (num % i == 0)
+                {
+                    sum += i + num / i;
+                }
+            }
+
+            sum++;
+            return sum == num;
+        }
+    }
+}
---
+++ FindRelativeRanks/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+/*
+ * 506. Relative Ranks
+ * https://leetcode.com/problems/relative-ranks/
+ */
+namespace FindRelativeRanks
+{
+    class Program
+    {
+        /*
+         * Given scores of N athletes, find their relative ranks and the people with the top three highest scores, who will be awarded medals: "Gold Medal", "Silver Medal" and "Bronze Medal".
+         * 
+         * Example 1:
+         * Input: [5, 4, 3, 2, 1]
+         * Output: ["Gold Medal", "Silver Medal", "Bronze Medal", "4", "5"]
+         * 
+         * Explanation: The first three athletes got the top three highest scores, so they got "Gold Medal", "Silver Medal" and "Bronze Medal". 
+         * For the left two athletes, you just need to output their relative ranks according to their scores.
+         * 
+         * Note:
+         * N is a positive integer and won't exceed 10,000.
+         * All the scores of athletes are guaranteed to be unique.
+         * 
+         */
+        static void Main(string[] args)
+        {
+            var result = FindRelativeRanks(new[] { 5, 4, 3, 2, 1 });
+        }
+
+        public static string[] FindRelativeRanks(int[] nums)
+        {
+            var index = new int[nums.Length];
+            var result = new string[nums.Length];
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                index[i] = i;
+            }
+
+            Array.Sort(index, (a, b) => nums[b] - nums[a]);
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (i == 0)
+                {
+                    result[index[i]] = "Gold Medal";
+                }
+                else if (i == 1)
+                {
+                    result[index[i]] = "Silver Medal";
+                }
+                else if (i == 2)
+                {
+                    result[index[i]] = "Bronze Medal";
+                }
+                else
+                {
+                    result[index[i]] = $"{i + 1}";
+                }
+            }
+
+            return result;
+        }
+    }
+}
